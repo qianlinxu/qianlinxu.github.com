a33ea57ae200fc1dd624847669d94fbb79c5bbfa
徐安琪 <xuanqi@joyasushis-MacBook-puro.local>
2019-01-24 20:51:03 -0500

    123456

---
+++ numbers.js
@@ -0,0 +1,57 @@
+
+document.getElementByid ("title").innerHTML="Welcome JavaScript";
+
+    var myNumber = 42;
+    myNumber=myNumber+10;
+
+    console.log(myNumber);  
+    console.log(myNumber+100); 
+    console.log(800)
+
+    var username = "Lin" ;
+    var bigNumber= myNumber >100 ;
+
+    console.log(bigNumber);
+    console.log(username+1122)
+
+    var users = ["Lin","Dan","Dave"];
+    console.log(users[0])
+
+    var complexUser = {name:"Lin ",zip:02120 , color:"red", dog:false ,
+    greet:function(greeting) {console.log(greeting="Lin")} 
+    };
+
+   complexUser.greet("goodbyl");
+    
+    console.log(complexUser.color);
+
+    var sayHello=function(){console.log("Hello");}
+
+    sayHello();
+
+    var sayHello=function(name){console.log("Hello"+name);}
+
+    sayHello("Lin");
+
+    var plusTen=function(num){return num+10;}
+    var thirty=plusTen(20);
+    console.log(20)
+
+    var thirty
+var newnum=20;
+    if (newnum>100) {console.log("bog number");
+    }
+
+    else{console.log("not big");
+    }
+
+
+    var greaterthanten=function(num){if (num>10){console.log("yes");}
+    else {console.log=("no");}
+    return num>10;
+    }
+
+var checknumber = greaterthanten(5);
+console.log(checknumber);
+
+
