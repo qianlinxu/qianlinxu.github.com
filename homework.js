0282e40a1e8d45e36576e2edc38b03566d0c61f4
徐安琪 <xuanqi@joyasushis-MacBook-puro.local>
2019-01-31 02:15:48 -0500

    23456789

--- homework.js
+++ homework.js
@@ -1,9 +1,18 @@
 var number = 1;
-var print_num = function(num){console.log(num)}
+var print_num = function(num)
+{console.log(num)}
 print_num(number);
 
 var change_number = function(num){num = 2}
 change_number(number);
 
 print_num(number);
+
+var 
+    if (number>100) {console.log("print_num");
+    }
+
+    else{console.log("not big");
+    }
+
 
