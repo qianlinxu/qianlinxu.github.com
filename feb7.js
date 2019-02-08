3579aaa11edf8ad014e0100dfb2e45808307e3dc
徐安琪 <xuanqi@joyasushis-MacBook-puro.local>
2019-02-07 20:09:01 -0500

    2345

---
+++ feb7.js
@@ -0,0 +1,17 @@
+console.log("Welcome to data land");
+
+d3.text("/data/text.txt",function(error,data)
+{console.log("error:",error);
+console.log("text:",data);
+});
+
+d3.csv("/data/csvfile.csv", function(error, data) {
+    console.log("csv:",data);
+});
+
+d3.json("/data/jsdata.json",function(error,data){
+console.log("error:",error);
+ console.log("json:",data);
+});
+
+console.log("end of line")
