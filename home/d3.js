fb5279dee792cb8355c891a0457b73fba0bc262f
徐安琪 <xuanqi@joyasushis-MacBook-puro.local>
2019-02-14 21:01:21 -0500

    23456

--- home/d3.js
+++ home/d3.js
@@ -6,7 +6,6 @@ title
 .style("color","red")
 .style("font-family","comic sans ms")
 
-
 var dataset=[50,43,89,100,150,167];
 console.log(dataset);
 var height=200
@@ -15,7 +14,6 @@ var svg=d3.select("#svg").append("svg").attr("height",height).attr("width",width
 var padding={top:20,left:20,right:20,bottom:20};
 var rectStep=35;
 var rectWidth=20;
-
 
 function Data(){
 var rectPosition= svg.selectAll("rect").data(dataset).enter().append("rect").attr("fill","rgb(255, 153, 255)")
@@ -26,12 +24,18 @@ var rectPosition= svg.selectAll("rect").data(dataset).enter().append("rect").att
 } 
 
 function Text(){
-var text=svg.selectAll("text").data(dataset).enter().append("text").attr("fill","black")
-.attr("font-size","14px")
-.attr("x",(d,i)=>padding.left+i*rectStep)
-.attr("y",(d,i)=>height-padding.bottom-d)
-.attr("width",rectWidth)
-.attr("height",d=>d);
+    var text=svg.selectAll("text").data(dataset)
+
+    text.enter().append("text").attr("fill","black")
+    .attr("font-size","14px")
+    .attr("x",(d,i)=>padding.left+i*rectStep)
+    .attr("y",(d,i)=>height-padding.bottom-d)
+    .attr("width",rectWidth)
+    .attr("height",d=>d);
+
+    text
+
+    text.exit()
 }
 
 
@@ -46,7 +50,6 @@ var DataUpdate=svg.selectAll("rect").data(dataset2).attr("fill","rgb(204, 153, 2
 .attr("y",(d,i)=>height-padding.bottom-d)
 .attr("width",rectWidth)
 .attr("height",d=>d);
-
 
 } ;
 
