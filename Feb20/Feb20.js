62d0de93ffec949974384af332115473022f91e5
徐安琪 <xuanqi@joyasushis-MacBook-puro.local>
2019-02-20 23:02:58 -0500

    1231

--- Feb20/Feb20.js
+++ Feb20/Feb20.js
@@ -4,3 +4,9 @@ d3.csv("/data/csvfile.csv", function(error, data) {
     console.log("Feb20/Feb20.csv:",data);
 });
 
+d3.csv("Feb20/Feb20.csv", function(error, data) {
+    data.forEach(function(d) {
+        d.export = parseFloat(d.export);
+    });
+    console.log("csv:", data);
+});
