d97f2048216cfc349195e35754cdf2b63bfafedd
徐安琪 <xuanqi@joyasushis-MacBook-puro.local>
2019-02-21 18:43:36 -0500

    12345678

--- Feb20/Feb20.js
+++ Feb20/Feb20.js
@@ -1,8 +1,6 @@
 console.log("Welcome");
 
-d3.csv("/data/csvfile.csv", function(error, data) {
-    console.log("Feb20/Feb20.csv:",data);
-});
+
 
 d3.csv("Feb20/Feb20.csv", function(error, data) {
     data.forEach(function(d) {
